abe916bb7626b586ef7548b0605e8c78edfd849d
xie-yu <2zluobo@163.com>
2012-08-06 00:39:32 +0800

    damn the ff

--- js/global.js
+++ js/global.js
@@ -33,7 +33,3 @@ function seeComments(thisAnchor) {
 	includeScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()}); 
 	return false;
 }
-
-function setFooter() {
-	document.getElementById("footer").style.top = document.height + "px";
-}
